dcdf3eb14686f816ed309901d8b59c4b863ca586
Damian <damiandanielvigo@gmail.com>
2019-12-11 21:21:12 -0300

    tipos de datos

---
+++ types-TypeInference-typeKeyword.ts
@@ -0,0 +1,27 @@
+let a: string = 'hola';
+a = 'Holas';
+a = 2; // es tipo string no se le puede asignar un numero
+
+let b: number = 10;
+b = a; // tipo numero no se le puede asignar ahora un string
+b = 20;
+b = 10 + 10;
+const num1 = 10;
+const num2 = 20;
+b = num1 + num2;
+
+function suma(num1: number, num2: number): number { // se le agrega un tipo a los parametros y tambien un tipo al retorno de la funcion
+  return num1 + num2;
+}
+let anyValue: any = 10;
+anyValue = 'Hola';
+
+suma(1,'2')
+
+function sumaJavascriptVanill(num1, num2) {
+  return num1 + num2;
+}
+
+type dni = string; // type keyword. es un alias
+let dniNumber: dni = 2123
+let dniNumber2: dni = 2123
